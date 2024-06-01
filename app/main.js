86fa50be7cfc6773c44bbf08923c008f02edc400
panman37 <baakejake@gmail.com>
2024-06-01 01:46:54 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,10 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.on("close", () => {
+        console.log('client close');
+        socket.end();
+        server.close();
+    });
 });
 
 server.listen(4221, "localhost");
