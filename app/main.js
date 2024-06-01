aa25849863f50a99814f0a2f29d83855c3cb11cd
panman37 <baakejake@gmail.com>
2024-06-01 10:23:28 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -6,9 +6,15 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        if(data.toString().split(" ")[1] === '/'){
+        request = data.toString()
+        url = request.split(" ")[1]
+        if(url == '/'){
             socket.write('HTTP/1.1 200 OK\r\n\r\n')
-        }else{
+        }else if(url.includes('/echo/')){
+            str = url.split('/').pop()
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`)
+        }
+        else{
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
         }
         socket.end();
