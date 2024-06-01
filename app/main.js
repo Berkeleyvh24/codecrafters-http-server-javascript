b536067ed9be37aa3173bf91317a45d6ae08c2ae
panman37 <baakejake@gmail.com>
2024-06-01 12:41:58 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -8,29 +8,34 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         request = data.toString()
+        console.log(request)
+        requestType = request.split(" ")[0]
+        console.log(requestType)
         url = request.split(" ")[1]
         console.log(process.argv[3])
+        let fileName = url.split('/').pop()
+        let directory = process.argv[3] 
         if(url == '/'){
             socket.write('HTTP/1.1 200 OK\r\n\r\n')
         }else if(url.includes('/echo/')){
-            let str = url.split('/').pop();
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`)
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${fileName.length}\r\n\r\n${fileName}`)
         }else if(url.includes('/user-agent')){
             let userString = 'User-Agent: '
             let str = request.substr(request.indexOf(userString)+userString.length, request.length);
             let userAgentResult = str.replace(/\s/g, "")
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentResult.length}\r\n\r\n${userAgentResult}`)
-        }else if(url.includes('/files/')){
-            let fileName = url.split('/').pop()
-            let directory = process.argv[3] 
-            console.log(fileName, '++++', directory)
+        }else if(url.includes('/files/') && requestType == 'GET'){
             if(fs.existsSync(directory + fileName)){
-                console.log('-----')
-                const data = fs.readFileSync(directory + fileName, 'utf8');
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`)
+                const content = fs.readFileSync(directory + fileName, 'utf8');
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
             }else{
                 socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
             }
+        }else if(url.includes('/files/') && requestType == 'POST'){
+            let postRequest = request.split("\r\n")
+            let content = postRequest[postRequest.length - 1]
+            fs.writeFileSync(directory + '/' + fileName, content)
+            socket.write('HTTP/1.1 201 Created\r\n\r\n')
         }
         else{
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
