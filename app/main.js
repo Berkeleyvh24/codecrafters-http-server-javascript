be43eadb3d067f8d66dba3f4a66c2fd7dbb1c3ec
panman37 <baakejake@gmail.com>
2024-06-01 23:36:15 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const net = require("net");
 const fs = require('node:fs');
+const zlib = require('zlib');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -12,17 +13,16 @@ const server = net.createServer((socket) => {
         url = request.split(" ")[1]
         let fileName = url.split('/').pop()
         let directory = process.argv[3] 
+        console.log(fileName, "+++++")
         if(url == '/'){
             socket.write('HTTP/1.1 200 OK\r\n\r\n')
         }else if(url.includes('/echo/')){
             let requestArray = request.split("\r\n")
             const encodingHeader = requestArray.find(e => e.includes('Accept-Encoding'))?.split(': ')[1];
-            let res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${fileName.length}\r\n\r\n${fileName}`
-            if(encodingHeader?.includes('gzip')){
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: ${fileName.length}\r\n\r\n${fileName}`)
-            }else{
-                socket.write(res)
-            }            
+            const acceptingHeader =  encodingHeader?.includes('gzip')
+            const compressOrUncompressString =  acceptingHeader ? zlib.gzipSync(fileName) : fileName;
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${compressOrUncompressString.length}\r\n${acceptingHeader ? 'Content-Encoding: gzip\r\n' : ''}\r\n`)
+            socket.write(compressOrUncompressString)
         }else if(url.includes('/user-agent')){
             let userString = 'User-Agent: '
             let str = request.substr(request.indexOf(userString)+userString.length, request.length);
