20180b72317b439914a560f040226d2b67eb2ee8
panman37 <baakejake@gmail.com>
2024-06-01 02:13:55 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,9 +5,13 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.on("close", () => {
-        console.log('client close');
+    socket.on("data", (data) => {
+        if(data.toString().split(" ")[1] === '/'){
+            socket.write('HTTP/1.1 200 OK\r\n\r\n')
+
+        }else{
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+        }
         socket.end();
         server.close();
     });
